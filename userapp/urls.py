483a41a8bcfe404d7a0e360cfaaf22d3807d3d86
Marcus Santos <msantos.gnu@gmail.com>
2016-05-12 00:28:09 -0300

    [login] Add Login path

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^userlogin/$', views.UserLogin.as_view()),
+    url(r'^$', views.UserLogin.as_view()),
     url(r'^logout/$', views.Logout.as_view()),
     url(r'^lista_usuarios/$', views.ListaUsuario.as_view()),
     url(r'^novo_usuario/$', views.CadastroUsuario.as_view()),
