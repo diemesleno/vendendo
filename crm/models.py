9d39567ac7f381b3a9f48e35abba511bede66951
Isis Soledad <contato@isissoledad.com>
2016-05-18 04:57:30 -0300

    [dashboard] Created model UserOrganization

--- crm/models.py
+++ crm/models.py
@@ -1,7 +1,18 @@
-from __future__ import unicode_literals
-
+# coding:utf-8
 from django.db import models
+from django.contrib import auth
+
 
 class Organization(models.Model):
-    user_account = models.ForeignKey('auth.User')
     name = models.CharField(max_length=100)
+    members = models.ManyToManyField('auth.User', through='UserOrganization')
+
+
+class UserOrganization(models.Model):
+    user_account = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    organization = models.ForeignKey('Organization', on_delete=models.CASCADE)
+    type_user_choices = ((u'A', u'Admin'),
+                         (u'M', u'Manager'),
+                         (u'S', u'Seller'))
+    type_user = models.CharField(max_length=1, choices=type_user_choices)
+    status_active = models.BooleanField(default=True)
