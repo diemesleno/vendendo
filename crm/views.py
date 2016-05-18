d03ed5138780caadeeb8ff5edca0fa8152a334fd
Isis Soledad <contato@isissoledad.com>
2016-05-17 22:34:06 -0300

    [dashboard] Created dashboard template

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import base, TemplateView
+from django.template.response import TemplateResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+class Dashboard(base.View):
+
+    template_name = "crm/dashboard.html"
+
+    def get(self, request):
+        return TemplateResponse(request,
+                                self.template_name,
+                                {})
