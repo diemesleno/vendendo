5f75f155d205d327f50bd0f20f40b0a316bc39e7
Isis Soledad <contato@isissoledad.com>
2016-05-20 11:27:30 -0300

    [dashboard] Updated menu

--- crm/views.py
+++ crm/views.py
@@ -4,6 +4,8 @@ from django.template.response import TemplateResponse
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from crm.models import UserOrganization
+from userapp.models import UserComplement
 
 
 class Dashboard(LoginRequiredMixin, base.View):
@@ -11,6 +13,15 @@ class Dashboard(LoginRequiredMixin, base.View):
     template_name = 'crm/dashboard.html'
 
     def get(self, request):
-      return TemplateResponse(request,
-                              self.template_name,
-                              {})
+        user_account = User.objects.get(id=request.user.id).id
+        organizations = UserOrganization.objects.filter(user_account=user_account)
+        organization_active = UserComplement.objects.get(user_account=user_account)
+        type_user_organization = UserOrganization.objects.get(
+                                    user_account=user_account, 
+                                    organization=organization_active.organization_active_id).type_user
+        return TemplateResponse(request,
+                                self.template_name,
+                                {'organizations': organizations,
+                                 'type_user_organization': type_user_organization,
+                                 'organization_active': organization_active,
+                                 })
