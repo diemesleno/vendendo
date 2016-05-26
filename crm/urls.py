2b3237645ae3b654b35d60b6c9f77d16772269a2
Marcus Santos <msantos.gnu@gmail.com>
2016-05-26 03:57:08 -0300

    [organization] correction of class based views for ListView and global session creation

--- crm/urls.py
+++ crm/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^organizations/$', views.Organizations.as_view()),
+    url(r'^organizations/$', views.OrganizationIndex.as_view(), name='organization_index'),
     url(r'^dashboard/$', views.Dashboard.as_view()),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import base, TemplateView
+from django.views.generic import base, ListView, TemplateView
 from django.template.response import TemplateResponse
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
@@ -8,43 +8,32 @@ from crm.models import UserOrganization
 from userapp.models import UserComplement
 
 
-class Dashboard(LoginRequiredMixin, base.View):
+class SessionMixin(object):
 
+    def get_context_data(self, **kwargs):
+        context = super(SessionMixin, self).get_context_data(**kwargs)
+        user_account = User.objects.get(id=self.request.user.id).id
+        organizations = UserOrganization.objects.filter(user_account=user_account)
+        organization_active = UserComplement.objects.get(user_account=user_account).organization_active
+        type_user_organization = UserOrganization.objects.get(
+                                     user_account=user_account, 
+                                     organization=organization_active).type_user
+        context['type_user_organization'] = type_user_organization
+        context['organization_active'] = organization_active
+        context['organizations'] = organizations
+        return context
+
+
+class Dashboard(LoginRequiredMixin, SessionMixin, TemplateView):
     template_name = 'crm/dashboard.html'
 
-    def get(self, request):
-        user_account = User.objects.get(id=request.user.id).id
-        organizations = UserOrganization.objects.filter(user_account=user_account)
-        organization_active = UserComplement.objects.get(user_account=user_account).organization_active
-        type_user_organization = UserOrganization.objects.get(
-                                    user_account=user_account, 
-                                    organization=organization_active).type_user
-        return TemplateResponse(request,
-                                self.template_name,
-                                {'organizations': organizations,
-                                 'type_user_organization': type_user_organization,
-                                 'organization_active': organization_active,
-                                 })
 
-
-class Organizations(LoginRequiredMixin, base.View):
-
+class OrganizationIndex(LoginRequiredMixin, SessionMixin, ListView):
     template_name = 'crm/listorganization.html'
+    context_object_name = 'my_organizations'
 
-    def get(self, request):
-        user_account = User.objects.get(id=request.user.id).id
-        organizations = UserOrganization.objects.filter(user_account=user_account)
-        organization_active = UserComplement.objects.get(user_account=user_account).organization_active
-        type_user_organization = UserOrganization.objects.get(
-                                    user_account=user_account, 
-                                    organization=organization_active).type_user
-        my_organizations = UserOrganization.objects.filter(user_account=user_account,
-                                                           type_user='A',
-                                                           status_active=True)
-        return TemplateResponse(request,
-                                self.template_name,
-                                {'organizations': organizations,
-                                 'type_user_organization': type_user_organization,
-                                 'organization_active': organization_active,
-                                 'my_organizations': my_organizations,
-                                 })
+    def get_queryset(self):
+        user_account = User.objects.get(id=self.request.user.id).id
+        return UserOrganization.objects.filter(user_account=user_account,
+                                               type_user='A',
+                                               status_active=True)
